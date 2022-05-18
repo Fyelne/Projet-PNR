0203567cd83520016224b35f2ad268aac1c5039d
matog7 <matau56100@gmail.com>
2022-05-18 08:57:12 +0200

    Harmonistaion App.java

--- src/View/App.java
+++ src/View/App.java
@@ -3,13 +3,11 @@ package View;
 import Controller.*;
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.event.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.*;
 import javafx.stage.Stage;
 
 public class App extends Application{
@@ -34,8 +32,6 @@ public class App extends Application{
             e.printStackTrace();
         }  
     }
-
-
  
  public static void main(String[] args) {
         launch(args);
