ab1870a294a2272adb2371a9cc1c3ed14610182d
Benji <guillome.e2101555@etud.univ-ubs.fr>
2022-05-25 10:28:16 +0200

    Benji

--- src/Modele/traitement/Graphe.java
+++ src/Modele/traitement/Graphe.java
@@ -46,7 +46,12 @@ public class Graphe {
     }
     
     public int nbAretes() {
-        return 0;
+        int ret = 0;
+        Set<Sommet> lessommets = this.sommetsVoisins.keySet();
+        for(Sommet som : lessommets){
+            ret = ret + (this.sommetsVoisins.get(som)).size();
+        }
+        return ret;
     }
     
     public boolean estDansGraphe(int idSom) {
