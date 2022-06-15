46d7b77e8ba126ffda49cec0328a79c52d7ea943
Fyelne <76248983+Fyelne@users.noreply.github.com>
2022-06-15 22:33:02 +0200

    Export SQL to CSV

---
+++ src/Controller/Export.java
@@ -0,0 +1,56 @@
+package Controller;
+
+
+import java.io.*;
+import java.sql.*;
+
+public class Export {
+    public static void exportTableToCSV(String tablename) {
+        try {
+            PrintWriter pw= new PrintWriter(new File("./" + tablename + ".csv"));
+            StringBuilder sb=new StringBuilder();
+            
+            
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_pnr", "root", "root");            
+            String query="select * from " + tablename;
+            Statement statement = connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery(query);
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            String[] columns = new String[columnsNumber];
+
+            for(int i = 1; i <= columnsNumber; i++){
+                if(i > 1){
+                    sb.append(",");
+                }
+                sb.append(rsmd.getColumnName(i));
+                columns[i-1]=rsmd.getColumnName(i);
+            }
+            sb.append("\r\n");
+
+            while(rs.next()){
+                for(int i = 1; i <= columnsNumber; i++){
+                    if(i > 1){
+                        sb.append(",");
+                    }
+                    sb.append(rs.getString(columns[i-1]));
+                }
+                sb.append("\r\n");
+            }
+            
+            pw.write(sb.toString());
+            pw.close();
+            System.out.println("Export fini");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void exportAllTableToCSV(String[] tablenames) {
+        for(String tablename : tablenames){
+            exportTableToCSV(tablename);
+        }
+    }
+}
